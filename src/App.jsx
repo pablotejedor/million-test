acae52753ffae0a7ab60c3afc205db23b536cf6b
Pablo <pablotejedor95@gmail.com>
2023-07-16 02:12:34 -0300

    colors added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import usePokemons from './hooks/usePokemons';
 import Buttons from './components/Buttons';
+import { colors } from './assets/colors';
 
-function App() {
+export default function App() {
      const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon');
 
      const { data, errors, loading } = usePokemons(url);
@@ -17,12 +18,12 @@ function App() {
                     <h3>Loading your data...</h3>
                ) : (
                     <div className="grid grid-cols-4 w-full min-h-full">
-                         {data &&
-                              data.listOfPokemons.map((pokemon) => (
-                                   <div
-                                        className="flex flex-col items-start py-5 px-10 bg-slate-300 m-5 rounded-xl cursor-pointer hover:scale-105"
-                                        key={pokemon.id}
-                                   >
+                         {data?.listOfPokemons.map((pokemon) => (
+                              <div
+                                   className="flex items-center justify-around py-5 px-10 bg-slate-300 m-5 rounded-xl cursor-pointer hover:scale-105"
+                                   key={pokemon.id}
+                              >
+                                   <div className="flex flex-col">
                                         <img
                                              className="w-36"
                                              src={
@@ -32,18 +33,41 @@ function App() {
                                              }
                                              alt={pokemon.name}
                                         />
-                                        <h3 className="text-2xl">
+                                        <h3 className="text-xl">
                                              #{pokemon.id}{' '}
                                              {`${pokemon.name[0].toUpperCase()}${pokemon.name.slice(
                                                   1
                                              )}`}
                                         </h3>
                                    </div>
-                              ))}
+
+                                   {/* Badges */}
+                                   <div className="flex items-center">
+                                        {pokemon.types.map((type) => {
+                                             return (
+                                                  <div
+                                                       className={
+                                                            'rounded-lg px-6 py-1 text-white'
+                                                       }
+                                                       style={{
+                                                            backgroundColor:
+                                                                 colors[
+                                                                      type.type
+                                                                           .name
+                                                                 ],
+                                                       }}
+                                                  >
+                                                       {type.type.name[0].toUpperCase()}
+                                                       {type.type.name.slice(1)}
+                                                  </div>
+                                             );
+                                        })}
+                                   </div>
+                                   {/* Badges */}
+                              </div>
+                         ))}
                     </div>
                )}
           </div>
      );
 }
-
-export default App;
