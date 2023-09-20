981926174ca4d19dae255d8008a3e21682f5b3c4
Pablo <pablotejedor95@gmail.com>
2023-09-20 18:52:15 -0300

    dead code deleted

--- src/hooks/usePokemons.js
+++ src/hooks/usePokemons.js
@@ -1,23 +1,14 @@
-import { useEffect } from 'react';
 import { getPokemons } from '../services/getPokemons';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 export default function usePokemons(url) {
      const { data, isLoading, error } = useQuery({
           queryKey: [url],
-          queryFn: () => getPokemons(url).then((response) => response),
+          queryFn: () => getPokemons(url),
           refetchOnMount: false,
           refetchOnWindowFocus: false,
           staleTime: Infinity,
      });
-
-     const queryClient = useQueryClient();
-
-     useEffect(() => {
-          queryClient.invalidateQueries({
-               queryKey: [url],
-          });
-     }, [url]);
 
      return { data, isLoading, error };
 }
